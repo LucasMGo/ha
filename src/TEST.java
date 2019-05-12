e76f566c61c09967e465432ae41a7107e77fcd2c
lucas_gold99 <lucas_gold99@win.tu-berlin.de>
2019-05-12 04:31:39 +0200

    commit

--- src/TEST.java
+++ src/TEST.java
@@ -2,5 +2,9 @@ public class TEST {
 
     public static void main(String[] arg){
         System.out.println("TTTTTT");
+        System.out.println("TTTTTT");
+        System.out.println("TTTTTT");
+        System.out.println("TTTTTT");
+
     }
 }
